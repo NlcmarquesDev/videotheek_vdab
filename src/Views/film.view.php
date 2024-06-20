c944863c0f182f3af05d38b121e29d929e964ed0
NlcmarquesDev <nunolcmarques@gmail.com>
2024-06-20 08:43:26 +0200

    frontpages almost single page design is done

--- src/Views/film.view.php
+++ src/Views/film.view.php
@@ -4,28 +4,28 @@ include './src/Views/partials/topbar.php';
 ?>
 <div class="container py-4">
 
-    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
-        <div class="container-fluid py-5">
-            <h1 class="display-5 fw-bold"><?= $movie['titel'] ?></h1>
-            <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
 
-        </div>
-    </div>
 
     <div class="row align-items-md-stretch">
         <div class="col-md-6">
-            <div class="h-100 p-5 text-bg-dark rounded-3">
-                <h2><?= $movie['Series_Title'] ?></h2>
-                <h4>Sinapse</h4>
-                <p><?= $movie['Overview'] ?></p>
-                <p>Date realease - <?= $movie['Released_Year'] ?></p>
-                <p>IMDB RATING - <?= $movie['IMDB_Rating'] ?></p>
-                <p>Duration - <?= $movie['Runtime'] ?></p>
+            <div class="h-100 p-5 bg-body-tertiary border rounded-3 d-flex flex-column justify-content-center">
+                <h1 class="display-5 fw-bold mb-5 text-center"><?= $movie['Series_Title'] ?></h1>
+                <div class="mx-auto">
+
+                    <img src="<?= $movie['Poster_Link'] ?>" width="300" alt="">
+                </div>
             </div>
         </div>
         <div class="col-md-6">
-            <div class="h-100 p-5 bg-body-tertiary border rounded-3">
-                <div class="d-flex gap-3 mb-5">
+            <div class="h-100 p-5 text-bg-dark rounded-3 d-flex flex-column justify-content-between">
+                <h2 class="mb-3"><?= $movie['Series_Title'] ?></h2>
+                <h4><b> Sinapse</b> </h4>
+                <p><?= $movie['Overview'] ?></p>
+                <p><b>Date realease -</b> <?= $movie['Released_Year'] ?></p>
+                <p><b>IMDB RATING -</b> <?= $movie['IMDB_Rating'] ?></p>
+                <p><b>Duration -</b> <?= $movie['Runtime'] ?>.</p>
+                <p><b>Movies in Store -</b></p>
+                <div class="d-flex gap-3 mt-5 mb-2 ms-auto">
                     <span>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
                             <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
@@ -42,9 +42,10 @@ include './src/Views/partials/topbar.php';
                         </svg>
                     </span>
                 </div>
-
-                <button class="btn btn-outline-secondary" type="button">add Whishlist</button>
-                <button class="btn btn-outline-secondary" type="button">Rent Movie</button>
+                <div class="d-flex flex-column gap-3">
+                    <button class="btn btn-outline-secondary" type="button">add Whishlist</button>
+                    <button class="btn btn-lg btn-outline-secondary" type="button">Rent Movie</button>
+                </div>
             </div>
         </div>
     </div>
